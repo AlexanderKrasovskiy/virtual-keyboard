2ee7a43de9a0fee93e29534534524a1795ec1bac
AlexanderKrasovskiy <fuxik2007@gmail.com>
2022-05-07 18:19:07 +0500

    feat: implement physical keyboard logic

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import './styles/style.scss';
 import keysData from './js/support/keysData';
+import lettersData from './js/support/lettersData';
 
-// ============ FNs ====================
+// ============ FNs ==================================
 function getUserLang() {
   return localStorage.getItem('lang') || 'en';
 }
@@ -89,14 +90,24 @@ function composeKeyboard(data, lang) {
   return KEYBOARD;
 }
 
-// ============ Construction =================
-let currentLang = getUserLang(); // make LET !!!
-currentLang = 'ru';
+// ============ STATE =============================
+let currentLang = getUserLang();
 
+let isShift = false;
+let isCaps = false;
+let isAlt = false;
+let isCtrl = false;
+
+// ============ CONSTRUCTION ===========================
 const WRAPPER = composeBasicElement('div', ['wrapper']);
 const HEADING = composeBasicElement('h1', ['heading'], 'RSS Virtual Keyboard');
 const TEXTAREA = composeBasicElement('textarea', ['textarea']);
 const KEYBOARD = composeKeyboard(keysData, currentLang);
+const KEYBOARD_KEYS = KEYBOARD.querySelectorAll('div.keyboard__key');
+const KEY_CAPS = KEYBOARD.querySelectorAll('div.keyboard__key > div.keycap');
+const KEY_CAPS_LETTERS = KEYBOARD.querySelectorAll(
+  'div.keyboard__key > div.keycap > span'
+);
 const OS_TEXT = composeBasicElement(
   'p',
   ['info'],
@@ -108,9 +119,224 @@ const COMBO_TEXT = composeBasicElement(
   'Смена языка: Ctrl + Alt.'
 );
 
-TEXTAREA.textContent = '\tDELETE ME !!\n! pLEASE'; // DELETE !!!
-
 WRAPPER.append(HEADING, TEXTAREA, KEYBOARD, OS_TEXT, COMBO_TEXT);
 document.body.append(WRAPPER);
 
+// ================= HANDLERS =====================
+
+function updateStateFlags(e) {
+  isShift = e.shiftKey;
+  isAlt = e.altKey;
+  isCtrl = e.ctrlKey;
+  if (e.code === 'CapsLock') {
+    isCaps = !isCaps;
+  }
+}
+
+function updateLangStyles() {
+  KEY_CAPS.forEach((cap) => cap.classList.toggle('hidden'));
+}
+
+function handleLangCombo() {
+  if (isAlt && isCtrl) {
+    currentLang = currentLang === 'en' ? 'ru' : 'en';
+    updateLangStyles();
+    localStorage.setItem('lang', currentLang);
+  }
+}
+
+function applyLetterCaseStylesOnDown(e) {
+  if (
+    (e.code === 'ShiftLeft' && isCaps && !e.repeat) ||
+    (e.code === 'ShiftRight' && isCaps && !e.repeat)
+  ) {
+    KEY_CAPS_LETTERS.forEach((span) => {
+      if (isCaps && isShift && span.classList.contains('shiftCaps')) {
+        span.classList.remove('hidden');
+      } else {
+        span.classList.add('hidden');
+      }
+    });
+  } else if (e.code === 'CapsLock' && isCaps && !e.repeat) {
+    KEY_CAPS_LETTERS.forEach((span) => {
+      if (isCaps && span.classList.contains('caps')) {
+        span.classList.remove('hidden');
+      } else {
+        span.classList.add('hidden');
+      }
+    });
+  } else if (
+    (e.code === 'ShiftLeft' && !e.repeat) ||
+    (e.code === 'ShiftRight' && !e.repeat)
+  ) {
+    KEY_CAPS_LETTERS.forEach((span) => {
+      if (isShift && span.classList.contains('caseUp')) {
+        span.classList.remove('hidden');
+      } else {
+        span.classList.add('hidden');
+      }
+    });
+  }
+}
+
+function findLetter(keyCode) {
+  if (keyCode === 'Tab') {
+    return '\t';
+  }
+
+  let letterCase = 'caseDown';
+
+  if (isShift && isCaps) {
+    letterCase = 'shiftCaps';
+  } else if (isShift) {
+    letterCase = 'caseUp';
+  } else if (isCaps) {
+    letterCase = 'caps';
+  }
+
+  const letter = lettersData[keyCode][currentLang][letterCase];
+  return letter;
+}
+
+function updateTextValue(letter) {
+  const start = TEXTAREA.selectionStart;
+  const end = TEXTAREA.selectionEnd;
+
+  const textBeforeSelection = TEXTAREA.value.slice(0, start);
+  const textAfterSelection = TEXTAREA.value.slice(end);
+  const newText = textBeforeSelection.concat(letter, textAfterSelection);
+
+  TEXTAREA.value = newText;
+  TEXTAREA.selectionStart = start + 1;
+  TEXTAREA.selectionEnd = start + 1;
+}
+
+// function printTextareaState() {
+
+//   console.log(
+//     'selectionStart: ',
+//     TEXTAREA.selectionStart,
+//     'selectionEnd: ',
+//     TEXTAREA.selectionEnd,
+//     'textLength: ',
+//     TEXTAREA.textLength,
+//     'value: ',
+//     [...TEXTAREA.value]
+//   );
+// }
+
+function handleKeyDown(e) {
+  TEXTAREA.focus();
+
+  updateStateFlags(e); // Alt Ctrl Caps Shift
+
+  handleLangCombo(); // Ctrl + Alt
+
+  // Apply pushed key style
+  const pressedKey = [...KEYBOARD_KEYS].find((key) =>
+    key.classList.contains(e.code)
+  );
+  if (!pressedKey) {
+    return;
+  }
+  pressedKey.classList.add('pushed');
+
+  // Apply styles on Caps / Shift / Caps + Shift
+  applyLetterCaseStylesOnDown(e);
+
+  // Pass default action for some FN keys
+  if (
+    e.code === 'Space' ||
+    e.code === 'Backspace' ||
+    e.code === 'Delete' ||
+    e.code === 'Enter' ||
+    e.code === 'ArrowLeft' ||
+    e.code === 'ArrowUp' ||
+    e.code === 'ArrowDown' ||
+    e.code === 'ArrowRight'
+  ) {
+    return;
+  }
+
+  // Prevent text input for other FN keys
+  if (
+    e.code === 'ShiftLeft' ||
+    e.code === 'ShiftRight' ||
+    e.code === 'CapsLock' ||
+    e.code === 'ControlLeft' ||
+    e.code === 'ControlRight' ||
+    e.code === 'AltLeft' ||
+    e.code === 'AltRight'
+  ) {
+    e.preventDefault();
+    return;
+  }
+
+  // Pass hot keys for Ctrl + C / V / X / Z / Y ...
+  if (isCtrl) {
+    return;
+  }
+
+  // Prevent user lang text input
+  e.preventDefault();
+
+  // Update textarea value
+  const letter = findLetter(e.code);
+  updateTextValue(letter);
+}
+
+function removeLetterCaseStylesOnUp(e) {
+  if (
+    (e.code === 'ShiftLeft' && isCaps) ||
+    (e.code === 'ShiftRight' && isCaps)
+  ) {
+    KEY_CAPS_LETTERS.forEach((span) => {
+      if (isShift && span.classList.contains('caps')) {
+        span.classList.remove('hidden');
+      } else {
+        span.classList.add('hidden');
+      }
+    });
+    isShift = false;
+  } else if (e.code === 'CapsLock') {
+    KEY_CAPS_LETTERS.forEach((span) => {
+      if (!isCaps && span.classList.contains('caseDown')) {
+        span.classList.remove('hidden');
+      } else {
+        span.classList.add('hidden');
+      }
+    });
+  } else if (e.code === 'ShiftLeft' || e.code === 'ShiftRight') {
+    KEY_CAPS_LETTERS.forEach((span) => {
+      if (isShift && span.classList.contains('caseDown')) {
+        span.classList.remove('hidden');
+      } else {
+        span.classList.add('hidden');
+      }
+    });
+    isShift = false;
+  }
+}
+
+function handleKeyUp(e) {
+  // Prevent 1-st keyup on Caps
+  if (isCaps && e.code === 'CapsLock') {
+    return;
+  }
+
+  // Remove pushed key style
+  const pressedKey = [...KEYBOARD_KEYS].find((key) =>
+    key.classList.contains(e.code)
+  );
+  if (!pressedKey) {
+    return;
+  }
+  pressedKey.classList.remove('pushed');
+
+  // Remove styles on Caps / Shift / Caps + Shift
+  removeLetterCaseStylesOnUp(e);
+}
+
 // =============== Listeners =================
+document.addEventListener('keydown', handleKeyDown);
+document.addEventListener('keyup', handleKeyUp);
